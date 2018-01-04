7f29f5191b57dba5a78315796db599d59c731555
shakeel999 <shakeel999sourceus@gmail.com>
2018-01-04 04:49:57 -0800

    made new techtalks

---
+++ techtalks.php
@@ -0,0 +1,198 @@
+<?php
+/*
+Author name: <Your name>;
+E-mail ID:   <Your E-mail ID>
+Date:        <dd-mm-yyyy>
+*/
+/////////////////////////////////////////////////////////
+//include_once("variables.php");
+//include_once("checkLoginStatus.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Title</title>
+    <?php include_once("stylesAndFont.php"); ?>
+</head>
+<body>
+  	<!-- Left bar starts -->
+     <div class="left-bar">
+      <div class="image1">
+        <img src="images/dcentmass_png.png" class="img-fluid">
+      </div>
+      <ul>
+        <!-- User class="activeLink" to make links active -->
+        <li class="activeLink"><a href="#"><i class="material-icons">home</i><span class="menuText">Home</span></a></li>
+        <li><a href="#"><i class="material-icons">language</i><span class="menuText">DCent MASS</span></a></li>
+        <li><a href="#"><i class="material-icons">settings_applications</i><span class="menuText">Our Services</span></a></li>
+        <li><a href="#"><i class="material-icons">group_work</i><span class="menuText">Student Hub</span></a></li>
+        <li><a href="#"><i class="material-icons">library_books</i><span class="menuText">Courses</span></a></li>
+        <li><a href="#"><i class="material-icons">event</i><span class="menuText">Events</span></a></li>
+        <li><a href="#"><i class="material-icons">perm_media</i><span class="menuText">Media</span></a></li>
+        <!-- <li><a href="#">Forum</a></li> -->
+        <li><a href="#"><i class="material-icons">contacts</i><span class="menuText">Contact Us</span></a></li>
+        <!-- <li style="border-bottom:  2px solid #06f;"><a href="#"></a></li> -->
+        <!-- <li style="border-bottom: 2px solid #06f;"><a href="#">FAQs</a></li> -->
+      </ul>
+     </div>
+     <!-- Left bar ends -->
+<!-- Mobile bar starts here -->
+
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <ul> <!-- User class="activeLink" to make links active -->
+        <li><a href="#">Home</a></li>
+        <li><a href="#">DCent MASS</a></li>
+        <li><a href="#">Our Services</a></li>
+        <li><a href="#">Student Hub</a></li>
+        <li><a href="#">Courses</a></li>
+        <li><a href="#">Events</a></li>
+        <li><a href="#">Media</a></li>
+        <!-- <li><a href="#">Forum</a></li> -->
+        <li style="border-bottom:  2px solid #06f;"><a href="#">Contact Us</a></li>
+        <!-- <li style="border-bottom:  2px solid #06f;"><a href="#">FAQs</a></li> -->
+  </ul>
+</div>
+
+<span id="openNavID" style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
+<!-- Mobile bar ends here -->
+     <!-- Right bar starts -->
+     <div class="right-bars">
+     	<div class="righ-col-1"> <!-- User class="activeLink" to make links active -->
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 1</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 2</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 3</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 4</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 5</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 6</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 7</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 8</span></div></a>
+         
+      </div>
+     	<div class="righ-col-2"> <!-- User class="activeLink" to make links active -->
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 9</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 10</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 11</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 12</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 13</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 14</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 15</span></div></a>
+        <a href="#"><div class="rightMenuBlocks"><span class="linkText">Text 16</span></div></a>
+     	</div>
+     </div>
+     
+<div class="mainContainer" id="mainContainer">  <!-- DO NOT REMOVE THIS -->
+	<div class="container-fluid">
+<!-- MAIN CONTENT STARTS -->
+<!-- <span class="bgtext">Template</span> -->
+
+
+
+
+<!-- WRITE MAIN CONTENT HERE -->
+ <div class="jumbotron jumbotron-fluid shakeel-jumbotron ">
+  <div class="container">
+    <center><h2>Techtalks</h2></center>
+  </div>
+</div>
+
+
+<div class="container-fluid">
+  <span class="sab-span1"><h2>Recent Talks</h2></span>
+  <div class="row">
+  <div class="col-md-6 sab-col">
+    <div class="card sab-card">
+      <div class="image-class">
+  
+    <img src="images/s6.jpeg" class="img-fluid">
+  </div>
+     <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.&nbsp;<a href="#"><span class="sab-span2"> MORE</span></a></p>
+     </div>
+  </div>
+</div>
+  <div class="col-md-6 sab-col">
+    <div class="card sab-card">
+    <div class="image-class">
+    <img src="images/s1.jpeg " class="img-fluid">
+  </div>
+     <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.&nbsp;<a href="#"><span class="sab-span2"> MORE</span></a></p>
+     </div>
+  </div>
+  </div>
+</div>
+</div>
+
+
+<span class=sab-span1><h2><span>DcentMASS</span> Is a Certified Learning Center</h2> </span>
+
+<div class="container-fluid sab-container1">
+   <div class="row">
+  <div class="col-md-12 sab-col1" >
+      <div class="card sab-card">
+        <div class="image-class">
+ <img src="images/s11.jpeg" class="img-fluid">
+</div>
+<div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.&nbsp;<a href="#"><span class="sab-span2"> MORE</span></a></p>
+     </div>
+   </div>
+
+</div>
+</div>
+</div>
+<div class="container-fluid sab-container2" >
+  <div class="row">
+  <div class="col-md-4">
+    <div class="card sab-card">
+      <div class="image-class">
+  <img src="images/javacoding.jpeg" class="img-fluid">
+</div>
+  <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.&nbsp;<a href="#"><span class="sab-span2"> MORE</span></a></p>
+     </div>
+   </div>
+  </div>
+  <div class="col-md-4">
+    <div class="card sab-card">
+      <div class="image-class">
+  <img src="images/s4.jpeg" class="img-fluid">
+</div>
+  <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.&nbsp;<a href="#"><span class="sab-span2"> MORE</span></a></p>
+     </div>
+   </div>
+  </div>
+  <div class="col-md-4">
+    <div class="card sab-card">
+      <div class="image-class">
+  <img src="images/s3.jpeg" class="img-fluid">
+</div>
+  <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.&nbsp;<a href="#"><span class="sab-span2"> MORE</span></a></p>
+     </div>
+   </div>
+  </div>
+
+</div>
+</div>
+
+
+<!-- NOTE: Please keep your code clean + readable and add comments -->
+
+
+
+
+
+<!-- MAIN CONTENT ENDS -->
+	</div>
+</div>
+<?php include_once("script.php"); ?>
+</body>
+</html>
