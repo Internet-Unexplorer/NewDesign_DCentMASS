fb0cc3146fe1c1d04fd04565c676c546e16f42cf
Md.asagar <asagardesaisourceus@gmail.com>
2018-01-11 08:14:22 +0530

    jobfair accept this one

--- jobFair.php
+++ jobFair.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Author name: <Your name>;
+Author name: Asgar
 E-mail ID:   <Your E-mail ID>
 Date:        <dd-mm-yyyy>
 */
@@ -16,16 +16,36 @@ Date:        <dd-mm-yyyy>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Title</title>
+    <title>Jobfair</title>
     <?php include_once("stylesAndFont.php"); ?>
 </head>
 <body>
+  <!-- Mobile bar starts here -->
+
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <ul> <!-- User class="activeLink" to make links active -->
+        <li><a href="#">Home</a></li>
+        <li><a href="#">DCent MASS</a></li>
+        <li><a href="#">Our Services</a></li>
+        <li><a href="#">Student Hub</a></li>
+        <li><a href="#">Courses</a></li>
+        <li><a href="#">Events</a></li>
+        <li><a href="#">Media</a></li>
+        <!-- <li><a href="#">Forum</a></li> -->
+        <li style="border-bottom:  2px solid #06f;"><a href="#">Contact Us</a></li>
+        <!-- <li style="border-bottom:  2px solid #06f;"><a href="#">FAQs</a></li> -->
+  </ul>
+</div>
+
+<span id="openNavID" style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
+<!-- Mobile bar ends here -->
 
   	
      
 <!-- DO NOT REMOVE THIS -->
-<!-- <div class="mainContainer" id="mainContainer">  
-	<div class="container-fluid"> -->
+<div class="mainContainer" id="mainContainer">  
+	<div class="container-fluid">
 <!-- MAIN CONTENT STARTS -->
  
 
@@ -166,8 +186,8 @@ Date:        <dd-mm-yyyy>
 
 
 <!-- MAIN CONTENT ENDS -->
-<!-- 	</div>
-</div> -->
+	</div>
+</div>
 <?php include_once("script.php"); ?>
 </body>
 </html>
